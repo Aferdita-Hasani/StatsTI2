4b3d373bc009e87038493b5eedaead3d4b25e336
ditahasani22@gmail.com <ditahasani22@gmail.com>
2019-11-25 14:02:22 +0100

    Add project files.

---
+++ StatsTI2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StatsTI2.MvcApplication" Language="C#" %>
---
+++ StatsTI2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StatsTI2.Startup))]
+namespace StatsTI2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
