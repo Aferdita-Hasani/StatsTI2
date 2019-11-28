acb5ee5d6f0bb299bf0e5d595406770efb356796
ditahasani22@gmail.com <ditahasani22@gmail.com>
2019-11-28 14:39:33 +0100

    Largimi i class library-ve Bo, Bll dhe  Dal

--- StatsTI2/Controllers/HomeController.cs
+++ StatsTI2/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace StatsTI2.Controllers
     {
         public ActionResult Index()
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
             return View();
         }
 
